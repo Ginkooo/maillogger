def0a9184eb034b8afe3ca59aacac0f529aaebd0
Piotr Czajka <piotr_czajka@outlook.com>
2017-07-11 14:16:50 +0200

    Really fixed

--- get_handler.py
+++ get_handler.py
@@ -1,24 +1,20 @@
-from os import path, chdir
-try:
-    from os import scandir
-except ImportError:
-    from scandir import scandir
+import os
 
 
 def get_logs(logdir):
-    currdir = path.abspath(path.curdir)
-    chdir(logdir)
+    currdir = os.path.abspath(os.path.curdir)
+    os.chdir(logdir)
     ret = ''
-    files = scandir()
-    for direntry in files:
+    files = os.listdir()
+    for f_name in files:
         try:
-            year, month, day = direntry.name.split('-')
+            year, month, day = f_name.split('-')
         except:
             pass
-        with open(direntry.name, 'r') as f:
+        with open(f_name, 'r') as f:
             for line in f:
                 time, ip, msg = line.strip().split(';')
-                ret += direntry.name + ';' + time + ';' + ip +\
+                ret += f_name + ';' + time + ';' + ip +\
                     ';' + msg + '\r\n'
-    chdir(currdir)
+    os.chdir(currdir)
     return ret
